9cd10a9ca0b42d3d5dd8d2b2cd4a874d93335b35
tfortner_banyanLabs <212409227+tfortner-banyanlabs@users.noreply.github.com>
2025-07-11 10:01:34 -0500

    'updated files'

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,5 +1,6 @@
 // lib/supabaseClient.ts
-import { createClient } from "@supabase/supabase-js";
+import { createClient as createClient } from "@supabase/supabase-js";
+
 
 // these env vars must exist in .env.local
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
@@ -7,3 +8,5 @@ const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 // create and export a single client instance
 export const supabase = createClient(url, anonKey);
+ 
+export { createClient }
